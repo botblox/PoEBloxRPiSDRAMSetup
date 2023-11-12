25b2f6d112e528f6fabd85007c71f5b639da3445
Aaron Elijah <aaronzakelijah@googlemail.com>
2023-11-12 17:25:51 +0000

    feat: enable Parity Programming
    - Not implemented SRAM or Parity programming in safe mode yet

--- tps23881_setup.py
+++ tps23881_setup.py
@@ -1,33 +1,21 @@
+import argparse
 import logging
 import struct
 from time import sleep
 
 from smbus2 import SMBus
 
+logging.basicConfig()
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 bus = SMBus(0)
 # 0x21 for channels 5-8 if device is in I2C 'Configuration B'
 # Default is Configuration A where device is addressed as a single I2C device with A0=0.
 # Note that SRAM and parity programming for upper channels 5-8 does not program SRAM or parity.
 i2c_address = 0x20 
-is_parity_enabled = False
 
-if __name__ == '__main__':
-    logger.info("SRAM and Parity programming steps for TPS23881")
-    logger.info(f"Parity enabled: {is_parity_enabled}")
-
-    bus.write_byte_data(i2c_address, 0x60, 0x01) # Reset the memory address pointer
-    bus.write_byte_data(i2c_address, 0x62, 0x00) # Set start address LSB
-    bus.write_byte_data(i2c_address, 0x63, 0x80) # Set start address MSB
-
-    if is_parity_enabled:
-        logger.exception("Parity is not implemented for TPS23881")
-        raise Exception("Parity is not implemented for TPS23881")
-    
-    bus.write_byte_data(i2c_address, 0x60, 0xC0) # Keep CPU in reset and enable SRAM I2C write
-
-    # prepare for RAM download
+def prepare_ram_download():
     bus.write_byte_data(i2c_address, 0x1D, 0xBC)
     bus.write_byte_data(i2c_address, 0xD7, 0x02)
     bus.write_byte_data(i2c_address, 0x91, 0x00)
@@ -35,8 +23,12 @@ if __name__ == '__main__':
     bus.write_byte_data(i2c_address, 0xD7, 0x00)
     bus.write_byte_data(i2c_address, 0x1D, 0x00)
 
-    # load SRAM data
-    sram_data = open('TPS23881_2_SRAM_v14.bin', 'rb').read()
+def set_start_address():
+    bus.write_byte_data(i2c_address, 0x62, 0x00) # Set start address LSB
+    bus.write_byte_data(i2c_address, 0x63, 0x80) # Set start address MSB
+
+def load_tps23881_binfile(filepath: str):
+    sram_data = open(filepath, 'rb').read()
     # offset 170 bytes to avoid unnecessary heading bytes
     sram_data = sram_data[170:]
     sram_data_bytes = list(struct.iter_unpack('c', sram_data))
@@ -51,7 +43,43 @@ if __name__ == '__main__':
             sram_data.append(int(_sram_bytestr, 2))
             _sram_bytestr = ''
         _sram_bytestr += sram_data_byte[0].decode("utf-8") 
-    # write SRAM data to TPS23881
+    
+    return sram_data
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--parity", help="enable Parity programming", action="store_true")
+    args = parser.parse_args()
+    
+    is_parity_enabled = True if args.parity else False
+
+    logger.info("SRAM and Parity programming steps for TPS23881")
+    logger.info(f"Parity enabled: {is_parity_enabled}")
+
+    bus.write_byte_data(i2c_address, 0x60, 0x01) # Reset the memory address pointer
+    set_start_address()
+
+    if is_parity_enabled:
+        bus.write_byte_data(i2c_address, 0x60, 0xC4) # Reset CPU and enable Parity Write
+        prepare_ram_download()
+
+        # load Parity data
+        parity_data = load_tps23881_binfile('TPS23881_2_PARITY_V14.bin')
+        # write Parity data
+        bus.write_block_data(i2c_address, 0x61, parity_data)
+
+        bus.write_byte_data(i2c_address, 0x60, 0xC5) # Keep CPU in reset and reset memory pointer
+        set_start_address()
+    
+    bus.write_byte_data(i2c_address, 0x60, 0xC0) # Keep CPU in reset and enable SRAM I2C write
+
+    # prepare for RAM download
+    if not is_parity_enabled:
+        prepare_ram_download()
+
+    # load SRAM data
+    sram_data = load_tps23881_binfile('TPS23881_2_SRAM_v14.bin')
+    # write SRAM data
     bus.write_block_data(i2c_address, 0x61, sram_data)
     
     if is_parity_enabled:
